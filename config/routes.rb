08b0cd0158fc54dc1e84057563d1b74ba85d33be
Sarah Howorka <showorka.mmt-b2011@fh-salzburg.ac.at>
2014-09-27 00:42:49 +0200

    added user controller, show user profile action, and list users (index) action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Webcasting::Application.routes.draw do
 
   resources :students
 
+  #resources :user
+
+  match 'user/:id' => 'user#show', :as => :user
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,7 +56,8 @@ Webcasting::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'students#index'
+  root :to => 'user#index'
+
 
   # See how all your routes lay out with "rake routes"
 
