0361e0649c740809cec06dd51174f11fc9d36780
Katharina <katharina.gruber1@gmail.com>
2015-01-09 17:43:36 +0100

    ''

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,8 @@
 
 # ruby encoding: utf-8
 
-User.create(name: 'Monika Bauer', email: 'monika@gmail.com', country: 'austria', city: 'vienna', birthdate: Date.today - 25.years, sex: 1, password:'password',
-            looking_for: 'Tanz (Breakdance, Hip – Hop), Akrobatik, Basketball, Fussball, American Football, Snowboarden, Ski-fahren, Klavier spielen, Licencse Klasse B',
-            description: 'We love to sightsee as much as possible. Mariusz is a fun of nature whereas. We like to meet new people, talk to them and learn about their life in other countries.')
-
-
-User.create(name: 'Franz Mueller', email: 'franz@gmail.com', country: 'austria', city: 'linz', birthdate: Date.today - 30.years, sex: 2,password:'password',
-            looking_for: 'Biomechanik nach Meyerhold Synchronsprechen Maskenspiel, Clownerie Fechten, Stockkampf Pantomime Reiten',
-            description: 'Im not a clubbing person. Sometimes I go out to restaurants, have some drinks, ..... but not to clubs / discos !! :)')
-
-User.create(name: 'Sophie Haider', email: 'sophie@gmail.com', country: 'austria', city: 'linz', birthdate: Date.today - 20.years, sex: 2,password:'password',
-            looking_for: 'Querfloete, Aereal Silk, klassisches Ballet, Jazzdance/ Contamporarydance, zeichnen, Fuehrerschein B',
-            description: 'My personality type is dramatic, expressive, proud, and demanding.')
-
-User.create(name: 'Hans Mitterlehner', email: 'hans@gmail.com', country: 'austria', city: 'vienna', birthdate: Date.today - 23.years, sex: 1,password:'password',
-            looking_for: 'Gitarre, Performance, Dancing, Experimental Sounddesign, Acting',
-            description: 'Only about 4percent of all people have my personality. I am Extroverted, Intuitive, Feeling, and Judging. ')
-
 University.create(name: 'Angewandte')
 University.create(name: 'Kunstuni Linz')
 University.create(name: 'Bruckner Uni')
+
+#User.create(name: 'Monika Bauer', email: 'monika@gmail.com', country: 'austria', city: 'vienna', sex: 1, password:'password', looking_for: 'Tanz (Breakdance, Hip – Hop), Akrobatik, Basketball, Fussball, American Football, Snowboarden, Ski-fahren, Klavier spielen, Licence Klasse B', description: 'We love to sightsee as much as possible. Mariusz is a fun of nature whereas. We like to meet new people, talk to them and learn about their life in other countries.')
