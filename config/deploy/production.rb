8f4990d6160dabcc424f16426346306db529466b
Sarah Howorka <showorka.mmt-b2011@fh-salzburg.ac.at>
2015-01-13 20:50:58 +0100

    cap fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,9 +6,9 @@ set :stage, :production
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+#role :app, %w{deploy@example.com}
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
 
 
 # Extended Server Syntax
@@ -17,7 +17,8 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '193.170.99.76', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '193.170.99.76', user: 'deploy', roles: %w{web app}
+#, my_property: :my_value
 
 
 # Custom SSH Options
