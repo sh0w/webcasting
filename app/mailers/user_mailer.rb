b3d796898127fcf23911d19d54ee9835b490bc8b
Katharina <katharina.gruber1@gmail.com>
2016-03-23 22:22:19 +0100

    mail neu?

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,12 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply_coact@ufg.at"
+
+  def send_blackboard_notification(user, post)
+    #@user = user
+    #@user.email
+    @post = post
+    mail( :to => 'katharina.gruber1@gmail.com',
+          :subject => 'New blackboard entry!' )
+  end
+
 end
